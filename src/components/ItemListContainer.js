be0f97d0f7cbc5ed571497dc4d5d7d2c4d2e36e2
Fiorella Pancini <fiorella.pancini@gmail.com>
2022-04-05 22:04:02 -0300

    Agregando el counter

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,19 +1,18 @@
+import ItemCount from "./ItemCount";
+
 const ItemListContainer = (props) => {
+
+    const onAdd = (qty) => {
+        alert("Has seleccionado " + qty + " imagenes.");
+    }
 
     return(  
         <>
-            <p>Aca va el {props.item}</p>  
+            <p>Aca va el {props.item}</p> 
+            <ItemCount stock={5} initial={1} onAdd={onAdd} />
         </>
         );
     
     }
     
     export default ItemListContainer;
-
-/* const Greeting = (props) => {
-    return (
-      <View style={{alignItems: 'center'}}>
-        <Text>Hello {props.name}!</Text>
-      </View>
-    );
-} */
---
+++ src/components/styledComponents.js
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+const ProductAmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin: 20px;
+`;
+
+const ProductAmount = styled.div`
+    font-size: 24px;
+    margin: 5px;
+`;
+
+export { ProductAmount, ProductAmountContainer }
